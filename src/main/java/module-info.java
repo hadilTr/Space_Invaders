8e2b37f31b173ee3c2d0f89b5c5aa481df971f46
YoussefSrasra <youssefsrasra8@gmail.com>
2025-11-20 20:22:26 +0100

    Initial commit: Structure de base JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module tn.client.space_invaders {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens tn.client.space_invaders to javafx.fxml;
+    exports tn.client.space_invaders;
+}
